d4823f898488cf1c3f09817f6795067c3e833873
Coderyounes <younesanas2021@gmail.com>
2024-04-18 09:08:13 +0100

    Task 10 : Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
-from typing import List, Tuple, Sequence, Iterable
+from typing import Iterable, Sequence, List, Tuple
+"""
+    `typing` module: Type hints for readable, maintainable code
+"""
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Calculate the length of each element in the given list.
+
+    Args:
+        lst: A list of sequences.
+
+    Returns:
+        A list of tuples, where each tuple contains
+        a sequence from the input list
+        and its corresponding length.
+
+    """
     return [(i, len(i)) for i in lst]
