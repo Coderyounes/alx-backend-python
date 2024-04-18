4b13e52e740f7a7cce3c75b8f34748e800a73c21
Coderyounes <younesanas2021@gmail.com>
2024-04-18 10:43:54 +0100

    Fix import order in safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Sequence, Any, Union
 """
     `typing` module: Type hints for readable, maintainable code
 """
+from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
