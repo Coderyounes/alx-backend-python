e04cacde692dfd8621f103d3a79a1a9df2412eb0
Coderyounes <younesanas2021@gmail.com>
2024-04-18 07:45:38 +0100

    Task 1 : Basic annotations - parameter annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes two floats as input and returns their sum.
+
+    Parameters:
+    a (int): The first integer.
+    b (int): The second integer.
+
+    Returns:
+    int: The sum of the two floats.
+    """
+    return a + b
