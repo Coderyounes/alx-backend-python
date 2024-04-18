e2c7cbaf6d33fcab41d8d74155cea9ff06b4f040
Coderyounes <younesanas2021@gmail.com>
2024-04-18 10:39:29 +0100

    Fix import statement in safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Mapping, Any, Union, TypeVar
 """
     `typing` module: Type hints for readable, maintainable code
 """
+from typing import Mapping, Any, Union, TypeVar
 
 
 T = TypeVar('T')
