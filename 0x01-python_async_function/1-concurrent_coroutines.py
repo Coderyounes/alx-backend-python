d8149db6804a58326f50ebcb50a9b4a71d63ed73
Coderyounes <younesanas2021@gmail.com>
2024-04-22 10:56:35 +0100

    Task 1: Execute Multiple coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+""" Documentation """
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Waits for random delays and returns a sorted list of the delays.
+
+    Args:
+        n (int): The number of delays to wait for.
+        max_delay (int): The maximum delay value.
+
+    Returns:
+        List[float]: A sorted list of the delays.
+    """
+    alldelays = []
+    for _ in range(n):
+        data = await wait_random(max_delay)
+        alldelays.append(data)
+    return sorted(alldelays)
