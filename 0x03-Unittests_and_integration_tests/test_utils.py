511a56f9d9964cab5cc2a74c6a03249f1774d303
Coderyounes <younesanas2021@gmail.com>
2024-05-09 15:46:25 +0100

    Task 2 : Mock HTTP calls , Using Mock & patch Module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ unittest: use parameterized to make it easy"""
 import unittest
+import requests
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -55,3 +57,34 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test case class for testing the get_json function.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """
+        Test the get_json function.
+
+        Args:
+            url (str): The URL to make the GET request to.
+            payload (dict): The expected JSON payload.
+
+        Returns:
+            None
+        """
+        with patch('requests.get') as mock_get:
+            mock_res = Mock()
+            mock_res.json.return_value = payload
+            mock_get.return_value = mock_res
+
+            res = get_json(url)
+
+            mock_get.assert_called_once_with(url)
+
+            self.assertEqual(res, payload)
